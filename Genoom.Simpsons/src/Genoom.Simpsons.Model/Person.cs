d1ad76360ff7a276f7cb8328767452d21873e972
Albert Giménez <albert.gimenez.quesada@gmail.com>
2017-03-20 23:40:43 +0100

    Initial project structure

---
+++ Genoom.Simpsons/src/Genoom.Simpsons.Model/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Genoom.Simpsons.Model
+{
+    public class Person
+    {
+        [Key]
+        private Guid Id { get; set; }
+        private string Name { get; set; }
+        private string Lastname { get; set; }
+        private DateTime Birthdate { get; set; }
+    }
+}

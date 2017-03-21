24bc13993a710315e48978f2ee5f2646b4e7198c
Albert Giménez <albert.gimenez.quesada@gmail.com>
2017-03-21 01:54:23 +0100

    Added mongoDb project

---
+++ Genoom.Simpsons/src/Genoom.Simpsons.Services.MongoDb/GetData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Genoom.Simpsons.Services.MongoDb
+{
+    public class GetData
+    {
+        public async Task<List<Model.Person>> GetPeople()
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("Genoom");
+            var collection = database.GetCollection<Model.Person>("SimpsonsFamilyTree");
+
+            return await collection.Find(x => x.Name == "Homer").ToListAsync();
+        }
+    }
+}

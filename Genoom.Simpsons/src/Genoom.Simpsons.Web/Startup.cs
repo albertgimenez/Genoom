70fd08e137a1dc691f588d5cc6252d60fc12fb04
Albert Giménez <albert.gimenez.quesada@gmail.com>
2017-03-22 02:27:08 +0100

    Configured sql server access Warning: dependency of ef in web project

--- Genoom.Simpsons/src/Genoom.Simpsons.Web/Startup.cs
+++ Genoom.Simpsons/src/Genoom.Simpsons.Web/Startup.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Genoom.Simpsons.Services.Sql;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -24,16 +26,20 @@ namespace Genoom.Simpsons.Web
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
+            services.AddDbContext<GenoomSimpsonsContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, GenoomSimpsonsContext context)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
             app.UseMvc();
+
+            DbInitializer.Initialize(context);
         }
     }
 }

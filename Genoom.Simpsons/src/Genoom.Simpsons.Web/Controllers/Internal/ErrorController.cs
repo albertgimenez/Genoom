d87f5e99c3b360d92cecfc270828b7e47bae0e61
Albert Giménez <albert.gimenez.quesada@gmail.com>
2017-03-23 00:51:14 +0100

    Updated Error Controller

--- Genoom.Simpsons/src/Genoom.Simpsons.Web/Controllers/Internal/ErrorController.cs
+++ Genoom.Simpsons/src/Genoom.Simpsons.Web/Controllers/Internal/ErrorController.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Net;
+﻿using System.Net;
 using System.Net.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Genoom.Simpsons.Web.Controllers
+namespace Genoom.Simpsons.Web.Controllers.Internal
 {
     public class ErrorController : Controller
     {

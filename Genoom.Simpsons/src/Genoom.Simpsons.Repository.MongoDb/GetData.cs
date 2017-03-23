b5a6e5bce51fceb0d3187fa8f4ad0733a3335176
Albert Giménez <albert.gimenez.quesada@gmail.com>
2017-03-23 20:47:56 +0100

    Added connection settings

--- Genoom.Simpsons/src/Genoom.Simpsons.Repository.MongoDb/GetData.cs
+++ Genoom.Simpsons/src/Genoom.Simpsons.Repository.MongoDb/GetData.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace Genoom.Simpsons.Repository.MongoDb

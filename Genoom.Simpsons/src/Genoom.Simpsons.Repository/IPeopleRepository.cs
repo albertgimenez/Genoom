afb46e992e4b1a20b1aa771c56964004961db66c
Albert Giménez <albert.gimenez.quesada@gmail.com>
2017-03-23 21:16:34 +0100

    Removed business logic solution it is not needed (still)

---
+++ Genoom.Simpsons/src/Genoom.Simpsons.Repository/IPeopleRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Genoom.Simpsons.Model;
+
+namespace Genoom.Simpsons.Repository
+{
+    public interface IPeopleRepository
+    {
+        // Read
+        Task<Person> ReadAsync(Guid id);
+        Task<IEnumerable<Person>> ReadFamilyAsync(Guid id);
+        Task<IEnumerable<Person>> ReadChildrenAsync(Guid id);
+        Task<IEnumerable<Person>> ReadTreeAsync(Guid id);
+        Task<bool> HasPartnerAsync(Guid id);
+
+        // Update
+        Task<Guid> AddChildrenAsync(Person child, Guid personId);
+    }
+}

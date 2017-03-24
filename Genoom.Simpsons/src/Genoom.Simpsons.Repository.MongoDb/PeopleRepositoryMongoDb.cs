b7485fb09c7f99f36c4c5cc7d17ba06ae105f6b5
Albert Giménez <albert.gimenez.quesada@gmail.com>
2017-03-24 02:55:18 +0100

    Added mongodb repo skeleton (not implemented yet)

---
+++ Genoom.Simpsons/src/Genoom.Simpsons.Repository.MongoDb/PeopleRepositoryMongoDb.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Genoom.Simpsons.Model;
+
+namespace Genoom.Simpsons.Repository.MongoDb
+{
+    public class PeopleRepositoryMongoDb : IPeopleRepository
+    {
+        #region Implementation of IPeopleRepository
+
+        public async Task<Person> GetPersonAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<PersonRelationship>> GetFamilyAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Person>> GetTreeAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> HasPartnerAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Guid> AddChildAsync(Person child, Guid personId)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}

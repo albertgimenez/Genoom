afc34b60b7633168ec9063946d32a76c80b421f6
Albert Giménez <albert.gimenez.quesada@gmail.com>
2017-03-26 22:33:59 +0200

    Added tests for PeopleRepositoryFactoryTest

---
+++ Genoom.Simpsons/tests/tests.u/Genoom.Simpsons.Web.Tests/Support/PeopleRepositoryFactoryTest.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Genoom.Simpsons.Repository.Sql;
+using Genoom.Simpsons.Repository.MongoDb;
+using Genoom.Simpsons.Web.Support;
+
+namespace Genoom.Simpsons.Web.Tests.Support
+{
+    [TestClass]
+    public class PeopleRepositoryFactoryTest
+    {
+        // Public Methods
+        /// <summary>
+        /// Intention: Test that we can get a Sql Repository
+        /// Expected: Success
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public void CreateSqlRepositoryOkTest()
+        {
+            //ARRANGE
+            var testConfig = TestConfiguration();
+            testConfig.GetSection("DbStrategy").Value="Sql";
+
+            //ACT
+            var testData = PeopleRepositoryFactory.Create(testConfig);
+
+            //ASSERT
+            Assert.IsNotNull(testData);
+            Assert.IsTrue(testData is PeopleRepositorySql);
+        }
+
+        // Public Methods
+        /// <summary>
+        /// Intention: Test that we can get a Sql Repository
+        /// Expected: Success
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public void CreateMongoDbRepositoryOkTest()
+        {
+            //ARRANGE
+            var testConfig = TestConfiguration();
+            testConfig.GetSection("DbStrategy").Value = "MongoDb";
+
+            //ACT
+            var testData = PeopleRepositoryFactory.Create(testConfig);
+
+            //ASSERT
+            Assert.IsNotNull(testData);
+            Assert.IsTrue(testData is PeopleRepositoryMongoDb);
+        }
+
+
+        // Private Methods
+        private IConfigurationRoot TestConfiguration()
+        {
+            var configBuilder = new ConfigurationBuilder();
+            configBuilder.AddJsonFile("appsettings.json");
+            return configBuilder.Build();
+        }
+    }
+}
